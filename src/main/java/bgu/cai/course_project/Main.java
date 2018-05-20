a8eaa298f0944f90a4c7e9fa60409274d41a1e57
daniel <daniel@daniel-Latitude-7490>
2018-05-20 23:03:04 +0300

    initializing project:
    added Log4J support
    added main class with example log line
    added basic run configuration

---
+++ src/main/java/bgu/cai/course_project/Main.java
@@ -0,0 +1,26 @@
+/**
+ * 
+ */
+package bgu.cai.course_project;
+
+import org.apache.log4j.Logger;
+
+/**
+ * @author daniel portnoy
+ *
+ */
+public class Main {
+	
+	private final static Logger LOGGER = Logger.getLogger(Main.class);
+
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		LOGGER.info("first log message :)");
+	}
+
+}
